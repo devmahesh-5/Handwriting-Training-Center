8680ec03cbfccd96a58e8aa1e75516811404c299
devmahesh-5 <n31mahesh@gmail.com>
2025-09-15 22:51:17 +0545

    file upload debug

--- src/utils/saveBuffer.ts
+++ src/utils/saveBuffer.ts
@@ -3,11 +3,24 @@ import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 import { ApiError } from '@/utils/ApiError';
 
+
+
+// let uploadedProfile = null;
+//         if (profilePicture) {
+//             const buffer = Buffer.from(await profilePicture.arrayBuffer());
+//             const tempFilePath = path.join(process.cwd(), "temp_" + uuidv4() + profilePicture.name);
+//             fs.writeFileSync(tempFilePath, buffer);
+//             uploadedProfile = await uploadOnCloudinary(tempFilePath);
+//             if (!uploadedProfile) {
+//                 throw new ApiError(500, "Failed to upload profile picture");
+//             }
+//         }
+
 export const saveBuffer = async(inputFile: File) => {
     try {
-        if (!inputFile || !(inputFile instanceof File)) {
-            throw new ApiError(404, "Invalid input file");
-        }
+        // if (!inputFile || !(inputFile instanceof File)) {
+        //     throw new ApiError(404, "Invalid input file");
+        // }
         const buffer = Buffer.from(await inputFile.arrayBuffer());
         const tempFilePath = path.join(process.cwd(), "temp_"+uuidv4() + inputFile.name);
         fs.writeFileSync(tempFilePath, buffer);
